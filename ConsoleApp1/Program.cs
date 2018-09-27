9d98464e9f0c76c4dc6a10b9e7340b420c775869
Asta <asta21ua@yahoo.com>
2018-09-27 13:27:28 +0400

    Homework1-1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int w = 24;
+            int z = 6;
+
+            int a = w + z;
+            int d = w - z;
+            int g = w * z;
+            int h = w / z;
+
+            Console.WriteLine($"w + z = {a}");
+            Console.WriteLine($"w - z = {d}");
+            Console.WriteLine($"w * z = {g}");
+            Console.WriteLine($"w / z = {h}");
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}

b62306fcc8018aa558ab9d89b972640df2198c6b
asta21ua <asta21ua@yahoo.com>
2018-11-07 19:52:34 +0400

    Class_1_1

---
+++ Classes/Class_1/Class_1_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World");
+
+
+            Console.ReadKey();
+        }
+    }
+}

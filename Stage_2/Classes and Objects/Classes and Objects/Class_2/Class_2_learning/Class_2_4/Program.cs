2a54d9cb9f800071b4d5a960ba402a5340ca3544
User <User@USER-PC>
2018-11-09 18:08:48 +0400

    Class_2_4 was added

--- Stage_2/Classes and Objects/Classes and Objects/Class_2/Class_2_learning/Class_2_4/Program.cs
+++ Stage_2/Classes and Objects/Classes and Objects/Class_2/Class_2_learning/Class_2_4/Program.cs
@@ -6,10 +6,55 @@ using System.Threading.Tasks;
 
 namespace Class_2_4
 {
+    class MyClass
+    {
+        private string field = null;
+
+        public string Field
+        {
+            set
+            {
+                if (value == "fool")
+                    Console.WriteLine("Incorrect value!");
+
+                else
+                    field = value;
+            }
+
+            get
+            {
+                if (field == null)
+                    return "There is no data in the field";
+                else if (field == "hello world")
+                    return field.ToUpper() + "!";
+                else
+                    return field;
+
+            }
+
+
+        }
+
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
+            MyClass instance = new MyClass();
+
+            instance.Field = "fool";
+            Console.WriteLine(instance.Field);
+
+
+            Console.WriteLine(new string('-', 50));
+
+            instance.Field = "hello world";
+            Console.WriteLine(instance.Field);                                      
+
+            Console.ReadKey();
+
         }
     }
 }

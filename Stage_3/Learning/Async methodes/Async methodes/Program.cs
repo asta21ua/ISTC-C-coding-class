ae598778c2724fb95e5067d6e7b44fbf91f22b74
PC <PC@PC->
2019-01-17 16:18:50 +0400

    creating Async methodes folder

---
+++ Stage_3/Learning/Async methodes/Async methodes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Async_methodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+        }
+    }
+}

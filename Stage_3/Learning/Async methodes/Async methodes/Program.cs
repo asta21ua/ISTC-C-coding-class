288344680cfc48ae62c00dae44524e4583527197
PC <PC@PC->
2019-01-17 17:28:37 +0400

    Async_01

--- Stage_3/Learning/Async methodes/Async methodes/Program.cs
+++ Stage_3/Learning/Async methodes/Async methodes/Program.cs
@@ -2,7 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+
+
+
+
 
 namespace Async_methodes
 {
@@ -10,10 +15,41 @@ namespace Async_methodes
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Primary flow: ID {0}", Thread.CurrentThread.ManagedThreadId);
 
+            var myDelegate = new Action(Method);
 
+            myDelegate.BeginInvoke(null, null);
+
+            Console.WriteLine("Main");
+            for (int i = 0; i < 80; i++)
+            {
+                Thread.Sleep(50);
+                Console.WriteLine(".");
+            }
+            Console.ReadKey();
+        }
+
+        static void Method()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\nAsync methode was lode.");
+            Console.WriteLine("\n Secondary flow: ID {0}", Thread.CurrentThread.ManagedThreadId);
+
+            for (int i = 0; i < 80; i++)
+            {
+                Thread.Sleep(60);
+                Console.WriteLine("A");
+
+            }
+
+              Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Async operation was done.\n");
+          
 
 
         }
+
+
     }
 }

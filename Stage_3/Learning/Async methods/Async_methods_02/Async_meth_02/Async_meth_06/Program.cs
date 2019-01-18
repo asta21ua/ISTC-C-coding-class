91fa2356c26cdf403f16e5511acb513ecec8fddf
PC <PC@PC->
2019-01-18 15:53:31 +0400

    The Folder Async_meth_06 was added

---
+++ Stage_3/Learning/Async methods/Async_methods_02/Async_meth_02/Async_meth_06/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Async_meth_06
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("Primary Flow: ID {0}", Thread.CurrentThread.ManagedThreadId);
+
+            var myDelagete = new Action(Method);
+
+            // the delaget method whitch will be launched after the asynchronous operation would end
+            var callback = new AsyncCallback(HandleCompletion);
+
+            myDelagete.BeginInvoke(callback, null);
+
+            Console.WriteLine("The Primary Flow is continuing to work");
+
+            //delay
+
+            Console.ReadKey();
+            
+        }
+        static void Method()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("\n Asynchronous methodv was lanched");
+            Console.WriteLine("\n Secondary Flow ID {0}", Thread.CurrentThread.ManagedThreadId);
+
+            for (int i = 0; i < 80; i++)
+            {
+                Thread.Sleep(30);
+                Console.Write(".");
+            }
+
+
+        }
+
+        static void HandleCompletion(IAsyncResult asyncResult)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Callback method Thread ID {0}", Thread.CurrentThread.ManagedThreadId);
+            Console.ForegroundColor = ConsoleColor.Red;
+            
+
+        }
+    }
+}

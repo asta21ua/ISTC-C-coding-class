28222958f25c457c0350ad6e92e5df55d593a1d3
PC <PC@PC->
2019-01-18 14:47:34 +0400

    Add in folder async_meth_04

--- Stage_3/Learning/Async methods/Async_methods_02/Async_meth_02/Async_meth_04/Program.cs
+++ Stage_3/Learning/Async methods/Async_methods_02/Async_meth_02/Async_meth_04/Program.cs
@@ -2,14 +2,47 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Async_meth_04
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            var myDelegate = new Func<int, int, int>(Add);
+
+            //launching of the asynchronous program
+
+            IAsyncResult asyncResult = myDelegate.BeginInvoke(1, 2, null, null);
+
+            Console.WriteLine("Asynchronous program was launched. The Main method is continuing to work");
+            Console.WriteLine("The Main method is wating for the end of the Asynchronous program launch");
+
+            Console.Writeline(asyncResult.AsyncWaitHandle.GetType());
+
+            asyncResult.AsyncWaitHandle.WaitOne();
+            Console.WriteLine("The end of Asynchronous method");
+
+            //getting the reslt of the Asynchronous oparetion
+            int result = myDelegate.EndInvoke(asyncResult);
+
+
+            //closing the waithHandle
+
+            asyncResult.AsyncWaitHandle.Close();
+            Console.WriteLine("Result =  "+ result);
+
+            //delay
+            Console.ReadKey();
+        }
+
+        static int Add(int a, int b)
+        {
+            Thread.Sleep(3000);
+            return a + b;
+            
         }
     }
 }
